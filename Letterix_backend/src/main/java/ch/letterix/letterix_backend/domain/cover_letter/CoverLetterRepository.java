70565cdcc607362a9943b7f780dab3e7deafdbf3
mikkaK <90750923+mikkaK@users.noreply.github.com>
2023-07-10 20:51:05 +0200

    Delete Letterix_backend/src/main directory

--- Letterix_backend/src/main/java/ch/letterix/letterix_backend/domain/cover_letter/CoverLetterRepository.java
+++
@@ -1,8 +0,0 @@
-package ch.letterix.letterix_backend.domain.cover_letter;
-
-import ch.letterix.letterix_backend.core.generic.AbstractRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CoverLetterRepository extends AbstractRepository<CoverLetter> {
-}
