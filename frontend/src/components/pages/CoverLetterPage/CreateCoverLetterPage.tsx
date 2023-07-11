ab142697eefbcb3a8ceae587a911fd8f66282d91
masha <masha.madani@gmail.com>
2023-07-11 09:42:58 +0200

    added validation

--- frontend/src/components/pages/CoverLetterPage/CreateCoverLetterPage.tsx
+++ frontend/src/components/pages/CoverLetterPage/CreateCoverLetterPage.tsx
@@ -4,7 +4,7 @@ import {useNavigate} from "react-router-dom";
 import CoverLetterForm from "../../organisms/CoverLetterForm";
 import GeneralBox from "../../molecules/GeneralBox";
 import CoverLetterService from "../../../Services/CoverLetterService";
-
+import * as Yup from 'yup';
 
 
 export const initialCoverLetterValues: any = {
@@ -20,10 +20,34 @@ export const initialCoverLetterValues: any = {
     weaknesses: '',
 };
 
+const validationSchema = Yup.object({
+    id: Yup.string()
+        .required('ID is required'),
+    name: Yup.string()
+        .required('Name is required'),
+    surname: Yup.string()
+        .required('Surname is required'),
+    age: Yup.string()
+        .required('Age is required'),
+    appliedCompany: Yup.string()
+        .required('Applied Company is required'),
+    position: Yup.string()
+        .required('Position is required'),
+    currentEducationLevel: Yup.string()
+        .required('Current Education Level is required'),
+    skills: Yup.string()
+        .required('Skills are required'),
+    interests: Yup.string()
+        .required('Interests are required'),
+    weaknesses: Yup.string()
+        .required('Weaknesses are required'),
+});
+
 const CoverLetter = () => {
     const navigate = useNavigate();
     const formik = useFormik({
         initialValues: initialCoverLetterValues,
+        validationSchema: validationSchema,
         onSubmit: (values) => {
             const coverLetter = values as CoverLetterModel;
             coverLetter.skills = values.skills.split(',');
@@ -32,6 +56,8 @@ const CoverLetter = () => {
             CoverLetterService.addCoverLetter(coverLetter).then((res) => {
                 navigate("/generatedCoverLetter/"+res.toString());
 
+            }).catch((res) => {
+                alert("Check your Inputs");
             });
         },
     });
